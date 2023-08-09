ac3198a892ba8f319b7f5199c2e4c8da163c1df8
adieri <136745251+adieri@users.noreply.github.com>
2023-08-09 12:52:35 -0400

    Delete action_page.php

--- action_page.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-  
-  Hi <?php echo htmlspecialchars($_POST['fname']); ?>.
-We will be contacting you at <?php echo htmlspecialchars($_POST['email']); ?>. very soon.
-
-  </body>
-</html>
