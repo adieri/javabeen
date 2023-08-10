6bdf89d9cbb57828ce932e56ab07f819d4372f7d
adieri <136745251+adieri@users.noreply.github.com>
2023-08-09 20:34:04 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,3 +40,15 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+/*responsive navigation */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
